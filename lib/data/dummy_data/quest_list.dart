09a9c58bdfbfcb25b44d00b070c501058414f273
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-09-19 22:25:39 -0300

    fix: added questions

--- lib/data/dummy_data/quest_list.dart
+++ lib/data/dummy_data/quest_list.dart
@@ -1,14 +1,104 @@
 import '../../entity/questions.dart';
 
 List<Question> questions = [
-  Question(title: "Titulo 1 Titulo 1 Titulo 1  ", listTitleResult: ["Res 1", "Res 2", "Res 3", "Res 4", "Res 5"], correctIndex: 0),
-  Question(title: "Titulo 2 Titulo 2 Titulo 2  ", listTitleResult: ["Res 1", "Res 2", "Res 3", "Res 4", "Res 5"], correctIndex: 1),
-  Question(title: "Titulo 3 Titulo 3 Titulo 3  ", listTitleResult: ["Res 1", "Res 2", "Res 3", "Res 4", "Res 5"], correctIndex: 2),
-  Question(title: "Titulo 4 Titulo 4 Titulo 4  ", listTitleResult: ["Res 1", "Res 2", "Res 3", "Res 4", "Res 5"], correctIndex: 3),
-  Question(title: "Titulo 5 Titulo 5 Titulo 5  ", listTitleResult: ["Res 1", "Res 2", "Res 3", "Res 4", "Res 5"], correctIndex: 0),
-  Question(title: "Titulo 6 Titulo 6 Titulo 6  ", listTitleResult: ["Res 1", "Res 2", "Res 3", "Res 4", "Res 5"], correctIndex: 1),
-  Question(title: "Titulo 7 Titulo 7 Titulo 7  ", listTitleResult: ["Res 1", "Res 2", "Res 3", "Res 4", "Res 5"], correctIndex: 2),
-  Question(title: "Titulo 8 Titulo 8 Titulo 8  ", listTitleResult: ["Res 1", "Res 2", "Res 3", "Res 4", "Res 5"], correctIndex: 3),
-  Question(title: "Titulo 9 Titulo 9 Titulo 9  ", listTitleResult: ["Res 1", "Res 2", "Res 3", "Res 4", "Res 5"], correctIndex: 0),
-  Question(title: "Titulo 10 Titulo 10 Titulo 10", listTitleResult: ["Res 1", "Res 2", "Res 3", "Res 4", "Res 5"], correctIndex: 1),
+  Question(
+    title: "Os stateless widgets são: ",
+    listTitleResult: [
+      "Os widgets que são destinados a consumir uma API",
+      "Os widgets incapazes de conter um estado",
+      "Os widgets capazes de conter um estado",
+      "Os widgets que não são visíveis",
+    ],
+    correctIndex: 1,
+  ),
+  Question(
+    title: "Dart é uma: ",
+    listTitleResult: [
+      "Linguagem de programação",
+      "Framework",
+      "Toolkit de Desenvolvimento",
+      "Base de dados",
+    ],
+    correctIndex: 0,
+  ),
+  Question(
+    title: "Flutter é um toolkit desenvolvido pela:",
+    listTitleResult: [
+      "Microsoft",
+      "Google",
+      "Sony",
+      "Canonical",
+    ],
+    correctIndex: 1,
+  ),
+  Question(
+    title: "O Dart foi criado com o objetivo de substituir: ",
+    listTitleResult: [
+      "PHP",
+      "Python",
+      "Java Script",
+      "Ruby",
+    ],
+    correctIndex: 2,
+  ),
+  Question(
+    title: "O Dart tem a estrutura baseada na linguagem: ",
+    listTitleResult: [
+      "Java",
+      "Kotlin",
+      "C#",
+      "C",
+    ],
+    correctIndex: 3,
+  ),
+  Question(
+    title: "O Flutter possui o ..... que é possivel fazer a visualização em tempo real do desenvolvimento.",
+    listTitleResult: [
+      "Hot reload",
+      "Debug",
+      "Widget Inspector",
+      "Explorador",
+    ],
+    correctIndex: 0,
+  ),
+  Question(
+    title: "No flutter podemos desenvolver para:",
+    listTitleResult: [
+      "Mobile",
+      "Desktop",
+      "Web",
+      "Todos",
+    ],
+    correctIndex: 3,
+  ),
+  Question(
+    title: "Importamos pacotes externos no flutter por meio do arquivo:",
+    listTitleResult: [
+      "main.dart",
+      "android/app/src/profile/AndroidManifest.xml",
+      "pubspec.yaml",
+      "pubspec.lock",
+    ],
+    correctIndex: 2,
+  ),
+  Question(
+    title: "O arquivo de entrada de um projeto flutter é o arquivo que possui a função:",
+    listTitleResult: [
+      "main() {...}",
+      "principal() {...}",
+      "startApp() {...}",
+      "init() {...}",
+    ],
+    correctIndex: 0,
+  ),
+  Question(
+    title: "O Widget já implementado no flutter responsável por definir uma página é o:",
+    listTitleResult: [
+      "Page();",
+      "Scaffold();",
+      "MaterialApp();",
+      "Screen();",
+    ],
+    correctIndex: 1,
+  ),
 ];
