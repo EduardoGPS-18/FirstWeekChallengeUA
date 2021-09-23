47c7e63b4cb7ccffaf0ea0281539a924157b5807
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-09-23 01:00:34 -0300

    refactor: remove responsibility of self set height

--- lib/widgets/question/header_question.dart
+++ lib/widgets/question/header_question.dart
@@ -1,45 +1,39 @@
 import 'package:flutter/material.dart';
 
 class HeaderQuestion extends StatelessWidget {
-  final BoxConstraints constraints;
   final String title;
 
   const HeaderQuestion({
     Key? key,
-    required this.constraints,
     required this.title,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: constraints.maxWidth,
-      height: constraints.maxHeight * 27 / 100,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Text(
-            title,
-            maxLines: 4,
-            overflow: TextOverflow.ellipsis,
-            style: const TextStyle(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Text(
+          title,
+          maxLines: 4,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(
+            fontSize: 22,
+          ),
+        ),
+        const Expanded(child: Center()),
+        const Padding(
+          padding: EdgeInsets.only(bottom: 12, top: 12),
+          child: Text(
+            "Respostas",
+            style: TextStyle(
               fontSize: 22,
+              color: Color(0xff758CFF),
             ),
           ),
-          const Expanded(child: Center()),
-          const Padding(
-            padding: EdgeInsets.only(bottom: 12, top: 12),
-            child: Text(
-              "Respostas",
-              style: TextStyle(
-                fontSize: 22,
-                color: Color(0xff758CFF),
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
